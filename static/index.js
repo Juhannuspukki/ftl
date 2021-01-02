fc3e41a2fe80b98c6d156606d42bf0e1c6fbc52b
Juhannuspukki <jere@jere.pro>
2021-01-02 22:08:44 +0200

    Create mobile navbar

--- static/index.js
+++ static/index.js
@@ -1,9 +1,12 @@
 // opens and closes the navbar
 const navBar = () => {
-  const x = document.getElementById("nav");
-  if (x.className === "navbar") {
+  const x = document.getElementById("header");
+  const links = document.getElementById("nav-links");
+  if (x.className === "ftl-stripe") {
     x.className += " responsive";
+    links.className += " responsive";
   } else {
-    x.className = "navbar";
+    x.className = "ftl-stripe";
+    links.className = "main-nav";
   }
 }
